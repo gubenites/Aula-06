20c50f22ddd330e314a2fbf09e38329a6cce1685
gubenites <gubenites99@gmail.com>
2022-09-13 11:26:38 -0300

    Atualizado

--- scripts/hello.py
+++ scripts/hello.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 from dev_aberto import hello
+from babel.dates import format_date
+
+import gettext
+gettext.install('cli', localedir='locale') 
 
 if __name__ == '__main__':
     date, name = hello()
-    print('Último commit feito em:', date, ' por', name)
+    print(_('Último commit feito em:'), format_date(date), _(' por'), name)
